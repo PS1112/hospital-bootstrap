2ee08844844d3fa4e5aac60eab2578dc2571f30e
Piyush <piyushrajput108@gmail.com>
2023-01-19 16:08:08 +0530

    home page complete

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import '../styles/footer.css'
 
 export default function Footer() {
   return (
-    <div>
-      footer
-    </div>
+  <>
+  <footer id='footer' className='col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'>
+<div id='row1' className='col-xs-12 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+  <h3>Our Doctors</h3>
+  <br/>
+  <br/>
+  <ul type='square'>
+  <li>&nbsp;Dr&nbsp;Awadesh&nbsp;Tripathi&nbsp;</li>
+  <li>&nbsp;Dr&nbsp;Karuna&nbsp;</li>
+  <li>&nbsp;Dr&nbsp;KVS&nbsp;Rana&nbsp;</li>
+  <li>&nbsp;Dr&nbsp;Yashlesh&nbsp;Tyagi&nbsp;</li>
+  <li>&nbsp;Dr&nbsp;Bhushan&nbsp;Mittal&nbsp;</li>
+  </ul>
+</div>
+<div id='row2' className='col-xs-12 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+  <h3>Opening Hours</h3>
+  <br/>
+  <ul type='squaare'>
+  <li>Mon&nbsp;:&nbsp;9:00&nbsp;AM&nbsp;-&nbsp;11:30&nbsp;PM&nbsp;</li>
+  <li>Tue&nbsp;:&nbsp;9:00&nbsp;AM&nbsp;-&nbsp;11:30&nbsp;PM&nbsp;</li>
+  <li>Wed&nbsp;:&nbsp;9:00&nbsp;AM&nbsp;-&nbsp;11:30&nbsp;PM&nbsp;</li>
+  <li>Thu&nbsp;:&nbsp;9:00&nbsp;AM&nbsp;-&nbsp;11:30&nbsp;PM&nbsp;</li>
+  <li>Fri&nbsp;:&nbsp;9:00&nbsp;AM&nbsp;-&nbsp;11:30&nbsp;PM&nbsp;</li>
+  <li>Sat&nbsp;:&nbsp;9:00&nbsp;AM&nbsp;-&nbsp;11:30&nbsp;PM&nbsp;</li>
+  <li>Sun&nbsp;:&nbsp;9:00&nbsp;AM&nbsp;-&nbsp;11:30&nbsp;PM&nbsp;</li>
+  </ul>
+</div>
+  </footer>
+  </>
   )
 }
--- src/components/home.js
+++ src/components/home.js
@@ -32,25 +32,52 @@ export default function Home() {
    <span className='h1'> About&nbsp;SP&nbsp;Hospital</span>
    <br/>
    <br/>
-   <span className='h4'>A Multi speciality Speciality Hospital, SP Hospital provides a comprehensive range of services to ensure that all of the patients get the high quality care they deserve. We have all the specialists under one roof.</span>
+   <span className='h5'>A Multi speciality Speciality Hospital, SP Hospital provides a comprehensive range of services to ensure that all of the patients get the high quality care they deserve. We have all the specialists under one roof.</span>
    <div id='floater' className='col-xs-12 col-12 col-sm-12 col-md-6 col-lg-7 col-xl-7'>
     <Button id='btn'><Link to='about_us'>Read more</Link></Button>
    </div>
   </div>
 </div>
-<div id='services_info' className='col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'>
-{/* <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */}
+<div id='services_info' className='col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'
+    data-aos="fade-up"
+    data-aos-offset="200"
+    data-aos-delay="30"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">
+<div id='col1'>
+  <span className='card-title'>
+    ICU    
+  </span>
+  <br/>
+  <span className='card-text'>
+  The Intensive Care Unit (ICU) is a separate, self-contained<br/> area within a medical facility, equipped with high-tech<br/> specialised facilities designed for close monitoring, rapid <br/>intervention and often extended treatment of patients with acute organ dysfunction.
+  </span>
+</div>
+
+<div id='col2'>
+  <span className='card-title'>
+    Vaccination
+  </span>
+  <br/>
+  <span className='card-text'>
+  Vaccination is the administration of a vaccine to help the immune system develop immunity from a disease.
+  </span>
+</div>
+
+<div id='col3'>
+  <span className='card-title'>
+    Laboratory
+  </span>
+  <br/>
+  <span className='card-text'>
+  healthcare facilities providing a wide range <br/>of laboratory procedures which aid the physicians<br/> in carrying out the diagnosis, treatment, and management of patients.
+  </span>
+</div>
+
 </div>
 <div id='query' className='col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'>
 <span className='h1'>Have any question or need any consultation? </span>
-<Button><Link to='about_us'>&nbsp;Contact&nbsp;Us&nbsp;</Link></Button>
+<Button id='btn'><Link to='about_us'>&nbsp;Contact&nbsp;Us&nbsp;</Link></Button>
 </div>
 
     </>
